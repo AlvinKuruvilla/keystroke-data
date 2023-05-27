8d954e6e5b3ecc5f3d9f3a58b4f7a6ae94716848
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-05-26 21:40:55 -0400

    finish up relative verifier impl
    actually do digraph disorder rather than unigraph disorder

--- src/verifier_tester.py
+++ src/verifier_tester.py
@@ -1,8 +1,11 @@
 from verifiers.similarity_verifier import SimilarityVerifier
-from verifiers.template_generator import create_kht_template_for_user_and_platform_id, create_kht_verification_attempt_for_user_and_platform_id
+from verifiers.relative_verifier import RelativeVerifier
+from verifiers.template_generator import create_kht_template_for_user_and_platform_id, create_kht_verification_attempt_for_user_and_platform_id, create_kit_flight_template_for_user_and_player, create_kit_flight_verification_attempt_for_user_and_player
 for i in range(1,27):
-    template = create_kht_template_for_user_and_platform_id(i,1)
-    verification = create_kht_verification_attempt_for_user_and_platform_id(i)
+    template = create_kit_flight_template_for_user_and_player(i,4)
+    verification = create_kit_flight_verification_attempt_for_user_and_player(i, 4)
 
     s_verifier = SimilarityVerifier(template, verification)
-    print(s_verifier.find_match_percent())
+    # print(s_verifier.find_match_percent())
+    r_verifier = RelativeVerifier(template, verification)
+    print(r_verifier.calculate_disorder(True))
--- src/verifiers/relative_verifier.py
+++ src/verifiers/relative_verifier.py
@@ -1,5 +1,7 @@
 def max_unigraph_disorder(n):
     return (n*n)-1
+def max_digraph_disorder(n):
+    return ((n*n)-1)/2
 class RelativeVerifier:
     def __init__(self, raw_template, raw_verification):
         self.template = raw_template
@@ -8,10 +10,16 @@ class RelativeVerifier:
         return list(set(self.template.keys()).intersection(set (self.verification.keys())))
     def keys_for_template_and_verification(self):
         return (list(self.template.keys()), list(self.verification.keys()))
-    def calculate_disorder(self):
+    def calculate_disorder(self, using_digraphs):
         matching_keys = self.get_all_matching_keys()
         disorder = 0
         template_keys, verification_keys = self.keys_for_template_and_verification()
-        for key in matching_keys:
-            disorder = disorder + abs(template_keys.index(key) - verification_keys.index(key))
-        return disorder/max_unigraph_disorder(len(matching_keys))
+        if using_digraphs == False:
+            for key in matching_keys:
+                disorder = disorder + abs(template_keys.index(key) - verification_keys.index(key))
+            return disorder/max_unigraph_disorder(len(matching_keys))
+        else:
+            for key in matching_keys:
+                disorder = disorder + abs(template_keys.index(key) - verification_keys.index(key))
+            return disorder/max_digraph_disorder((len(matching_keys)))
+
