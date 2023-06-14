aaa615aeded99979f08bc3b2af697951b40486d5
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-06-13 22:14:23 -0400

    Update heatmap test

--- src/heatmap_test.py
+++ src/heatmap_test.py
@@ -1,13 +1,18 @@
-from verifiers.heatmap import HeatMap, VerifierType
+from verifiers.heatmap import HeatMap, VerifierType, create_kit_data_from_df
+from verifiers.template_generator import read_compact_format
 
+df = read_compact_format()
 
-heatmap = HeatMap(VerifierType.SimilarityUnweighted)
-matrix = heatmap.make_kht_matrix(1, 1, None, None)
-print(matrix)
+res = create_kit_data_from_df(df, 1)
+print(res)
 input()
-heatmap.plot_heatmap(matrix, "FF")
-matrix = heatmap.make_kht_matrix(2, 2, None, None)
-heatmap.plot_heatmap(matrix, "II")
+heatmap = HeatMap(VerifierType.Similarity)
+matrix = heatmap.make_combined_kht_kit_matrix(1, 1, 1, 1, 1)
+print(matrix)
+# input()
+heatmap.plot_heatmap(matrix, "FF Combined (Flight 1) Similarity")
+# matrix = heatmap.make_kht_matrix(2, 2, None, None)
+# heatmap.plot_heatmap(matrix, "II")
 
-matrix = heatmap.make_kht_matrix(3, 3, None, None)
-heatmap.plot_heatmap(matrix, "TT")
+# matrix = heatmap.make_kht_matrix(3, 3, None, None)
+# heatmap.plot_heatmap(matrix, "TT")
