afe38469ed8351f0fb25a5adcbf926f6a6535634
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-07-01 23:39:05 -0400

    Move keystroke feature creation to its own file

--- src/linguistic_features_test.py
+++ src/linguistic_features_test.py
@@ -1,6 +1,9 @@
 import os
 from tqdm import tqdm
-from features.linguistic_features import estimate_shannon_entropy, plot_multi_bar_graph
+from features.linguistic_features import (
+    estimate_shannon_entropy,
+    plot_multi_bar_graph,
+)
 from features.ndtw import CSM
 
 from features.word_parser import get_user_by_platform, plot_heatmap, SentenceParser
@@ -28,17 +31,18 @@ def shannon_entropy_similarity():
     plot_multi_bar_graph(fb, insta, twitter)
 
 
-matrix = []
-for i in tqdm(range(1, 28)):
-    df = get_user_by_platform(i, 1)
-    sp = SentenceParser(os.path.join(os.getcwd(), "cleaned.csv"))
-    document_words = sp.get_words(df)
-    row = []
-    for j in range(1, 28):
-        df = get_user_by_platform(j, 2)
-        sp = SentenceParser(os.path.join(os.getcwd(), "cleaned.csv"))
-        query_words = sp.get_words(df)
-        csm = CSM(query_words, document_words)
-        row.append(csm.calculate_csm())
-    matrix.append(row)
-plot_heatmap(matrix, "CSM Facebook-Instagram")
+# matrix = []
+# for i in tqdm(range(1, 28)):
+#     df = get_user_by_platform(i, 1)
+#     sp = SentenceParser(os.path.join(os.getcwd(), "cleaned.csv"))
+#     document_words = sp.get_words(df)
+#     row = []
+#     for j in range(1, 28):
+#         df = get_user_by_platform(j, 1)
+#         sp = SentenceParser(os.path.join(os.getcwd(), "cleaned.csv"))
+#         query_words = sp.get_words(df)
+#         csm = CSM(query_words, document_words)
+#         row.append(csm.calculate_csm())
+#     matrix.append(row)
+# plot_heatmap(matrix, "CSM Facebook-Facebook")
+shannon_entropy_similarity()
--- src/verifiers/heatmap.py
+++ src/verifiers/heatmap.py
@@ -1,7 +1,7 @@
 import enum
-from collections import defaultdict
 import matplotlib.pyplot as plt
 import seaborn as sns
+from features.keystroke_features import create_kht_data_from_df, create_kit_data_from_df
 from src.verifiers.template_generator import read_compact_format
 from src.verifiers.verifiers import Verifiers
 
@@ -24,35 +24,6 @@ def get_user_by_platform(user_id, platform_id, session_id=None):
         & (df["platform_id"] == platform_id)
         & (df["session_id"] == session_id)
     ]
-
-
-def create_kht_data_from_df(df):
-    kht_dict = defaultdict(list)
-    for i, row in df.iterrows():
-        kht_dict[row["key"]].append(row["release_time"] - row["press_time"])
-    return kht_dict
-
-
-def create_kit_data_from_df(df, kit_feature_type):
-    kit_dict = defaultdict(list)
-    for i, g in df.groupby(df.index // 2):
-        if g.shape[0] == 1:
-            continue
-        key = g.iloc[0]["key"] + g.iloc[1]["key"]
-        initial_press = g.iloc[0]["press_time"]
-        second_press = g.iloc[1]["press_time"]
-        initial_release = g.iloc[0]["release_time"]
-
-        second_release = g.iloc[1]["release_time"]
-        if kit_feature_type == 1:
-            kit_dict[key].append(float(second_press) - float(initial_release))
-        elif kit_feature_type == 2:
-            kit_dict[key].append(float(second_release) - float(initial_release))
-        elif kit_feature_type == 3:
-            kit_dict[key].append(float(second_press) - float(initial_press))
-        elif kit_feature_type == 4:
-            kit_dict[key].append(float(second_release) - float(initial_press))
-    return kit_dict
 
 
 class HeatMap:
